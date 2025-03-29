b925d1e1a06d9bd1976825ce25d36a9224202651
Vitalij Nikonov <BarsStudio@gmail.com>
2025-03-29 23:24:33 +0800

    123

---
+++ ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+    
+    public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();   // создаем базу данных при первом обращении
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Score = 37 },
+                new User { Id = 2, Score = 41 }
+        );
+    }
+}
